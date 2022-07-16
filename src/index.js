9debae2204495e66c1cc3612c99b77cf82c94a4a
27Ivan0vS <IvanovSergey.94.28.@gmail.com>
2022-07-16 23:47:47 +0300

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openingBrackets = [];
+  const closingBrackets = [];
+  const expectedBracketList = [];
+  const bracketMap = {};
+
+  bracketsConfig.forEach(item => {
+    openingBrackets.push(item[0]);
+    closingBrackets.push(item[1]);
+    bracketMap[item[0]] = item[1];
+  })
+  //console.log('openingBrackets =', openingBrackets, bracketMap)
+
+  for (let i = 0; i < str.length; i++) {
+    const currentBracket = str[i];
+    if (openingBrackets.includes(currentBracket) && currentBracket !== expectedBracketList[expectedBracketList.length - 1]) {
+      expectedBracketList.push(bracketMap[currentBracket])
+      //console.log('expectedBracketList =', expectedBracketList)
+    } else {
+      const expectedBracket = expectedBracketList.pop();
+
+      //console.log('expectedBracket =', expectedBracket, 'currentBracket =', currentBracket)
+      if (expectedBracket !== currentBracket) {
+        return false;
+      }
+      //console.log('bracketMap.currentBracket =', bracketMap[currentBracket])
+    }
+  }
+
+  return expectedBracketList.length === 0 ? true : false;
 }
